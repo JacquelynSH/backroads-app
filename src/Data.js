19ebef21636f65d6f01fac41bfa2eee036816c8f
JacquelynSH <elpatchaembroidery@gmail.com>
2023-01-20 16:54:32 -0800

    Set up map function in Footer, Servies, and Tours components

--- src/Data.js
+++ src/Data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg';
+import tour2 from './images/tour-2.jpeg';
+import tour3 from './images/tour-3.jpeg';
+import tour4 from './images/tour-4.jpeg';
+
 export const pageLinks = [
   {id:1, href: '#home', text: 'home'},
   {id:2, href: '#about', text: 'about'},
@@ -9,4 +14,65 @@ export const socialLinks = [
   {id:1, href:'https://www.twitter.com', icon:'fab fa-facebook'},
   {id:2, href:'https://www.twitter.com', icon:'fab fa-twitter'},
   {id:3, href:'https://www.twitter.com', icon:'fab fa-squarespace'}
+];
+
+export const services = [
+  {id: 1, 
+  icon: 'fas fa-wallet fa-fw', 
+  title: 'saving money', 
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia'
+  },
+  {id: 2,
+  icon: 'fas fa-tree fa-fw', 
+  title: 'endless hiking', 
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia'
+  },
+  {id: 3, 
+  icon: 'fas fa-socks fa-fw', 
+  title: 'amazing comfort', 
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia'
+  }
+];
+
+export const tours = [
+  {
+    id: 1, 
+    title: 'Tibet Adventure', 
+    img: tour1, 
+    date: 'august 26th, 2020', 
+    text: '  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    city: 'china',
+    days: '6 days',
+    price: 'from $2100'
+  },
+  {
+    id: 2, 
+    title: 'best of java', 
+    img: tour2, 
+    date: 'october 1th, 2020', 
+    text: '  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    city: 'indonesia',
+    days: '11 days',
+    price: 'from $1400'
+  },
+  {
+    id: 3, 
+    title: 'explore hong kong', 
+    img: tour3, 
+    date: 'september 15th, 2020', 
+    text: '  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    city: 'hong kon',
+    days: '8 days',
+    price: 'from $5000'
+  },
+  { 
+    id: 4, 
+    title: 'kenya highlights', 
+    img: tour4, 
+    date: 'december 5th, 2019', 
+    text: '  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    city: 'kenya',
+    days: '20 days',
+    price: 'from $3300'
+  }
 ]
